1a8445d5afc6c87a354b13f70da7beb438068928
Lauren <lauren.baird@gmail.com>
2014-05-31 14:55:45 +1000

    Update database_structure.sql
    updated missing auto increment statements

--- database_structure.sql
+++ database_structure.sql
@@ -1,248 +1,251 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
-
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`country`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`country` (
-  `country_id` INT(3) NOT NULL ,
-  `country_name` VARCHAR(250) NULL ,
-  `country_code` VARCHAR(3) NULL ,
-  PRIMARY KEY (`country_id`) )
-ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table `mydb`.`village`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`village` (
-  `village_id` INT(11) NOT NULL ,
-  `village_name` VARCHAR(45) NULL ,
-  `country_id` INT(3) NULL ,
-  PRIMARY KEY (`village_id`) ,
-  INDEX `country_id_fk` (`country_id` ASC) ,
-  CONSTRAINT `country_id_fk`
-    FOREIGN KEY (`country_id` )
-    REFERENCES `mydb`.`country` (`country_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `audio`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`project`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`project` (
-  `project_id` INT(11) NOT NULL ,
-  `project_name` VARCHAR(250) NOT NULL ,
-  `facebook_pageid` INT(100) NULL ,
-  PRIMARY KEY (`project_id`) )
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `audio` (
+  `audio_id` int(11) NOT NULL AUTO_INCREMENT,
+  `audio_title` varchar(45) DEFAULT NULL,
+  `audio_desc` varchar(500) DEFAULT NULL,
+  `audio` blob,
+  PRIMARY KEY (`audio_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `mydb`.`user`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`user` (
-  `user_id` INT(11) NOT NULL ,
-  `username` VARCHAR(45) NOT NULL ,
-  `facebook_email` VARCHAR(200) NULL ,
-  PRIMARY KEY (`user_id`) )
-ENGINE = InnoDB;
+--
+-- Table structure for table `comments`
+--
 
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment` varchar(500) NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- -----------------------------------------------------
--- Table `mydb`.`project_story`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`project_story` (
-  `story_id` INT(11) NOT NULL ,
-  `story_name` VARCHAR(250) NULL ,
-  `project_id` INT(11) NOT NULL ,
-  `village_id` INT(11) NOT NULL ,
-  `latitude` DECIMAL(10,7) NULL ,
-  `longitude` DECIMAL(10,7) NULL ,
-  `submitted_by` INT(11) NULL ,
-  `project_desc` VARCHAR(2000) NULL ,
-  PRIMARY KEY (`story_id`) ,
-  INDEX `village_id_fk` (`village_id` ASC) ,
-  INDEX `project_id_fk` (`project_id` ASC) ,
-  INDEX `submitted_by_fk` (`submitted_by` ASC) ,
-  CONSTRAINT `village_id_fk`
-    FOREIGN KEY (`village_id` )
-    REFERENCES `mydb`.`village` (`village_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `project_id_fk`
-    FOREIGN KEY (`project_id` )
-    REFERENCES `mydb`.`project` (`project_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `submitted_by_fk`
-    FOREIGN KEY (`submitted_by` )
-    REFERENCES `mydb`.`user` (`user_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `country`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`tags`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`tags` (
-  `tag_id` INT(11) NOT NULL ,
-  `tag_name` VARCHAR(30) NULL ,
-  PRIMARY KEY (`tag_id`) )
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `country` (
+  `country_id` int(3) NOT NULL AUTO_INCREMENT,
+  `country_name` varchar(250) DEFAULT NULL,
+  `country_code` varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`country_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `mydb`.`comments`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`comments` (
-  `comment_id` INT(11) NOT NULL ,
-  `comment` VARCHAR(500) NOT NULL ,
-  PRIMARY KEY (`comment_id`) )
-ENGINE = InnoDB;
+--
+-- Table structure for table `picture`
+--
 
+CREATE TABLE IF NOT EXISTS `picture` (
+  `picture_id` int(11) NOT NULL AUTO_INCREMENT,
+  `picture_title` varchar(45) DEFAULT NULL,
+  `picture_desc` varchar(500) DEFAULT NULL,
+  `picture` blob,
+  `flickr_id` int(100) DEFAULT NULL,
+  PRIMARY KEY (`picture_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- -----------------------------------------------------
--- Table `mydb`.`picture`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`picture` (
-  `picture_id` INT NOT NULL ,
-  `picture_title` VARCHAR(45) NULL ,
-  `picture_desc` VARCHAR(500) NULL ,
-  `picture` BLOB NULL ,
-  `flickr_id` INT(100) NULL ,
-  PRIMARY KEY (`picture_id`) )
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `project`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`audio`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`audio` (
-  `audio_id` INT NOT NULL ,
-  `audio_title` VARCHAR(45) NULL ,
-  `audio_desc` VARCHAR(500) NULL ,
-  `audio` BLOB NULL ,
-  PRIMARY KEY (`audio_id`) )
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `project` (
+  `project_id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(250) NOT NULL,
+  `facebook_pageid` int(100) DEFAULT NULL,
+  PRIMARY KEY (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `mydb`.`video`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`video` (
-  `video_id` INT(11) NOT NULL ,
-  `video_title` VARCHAR(45) NULL ,
-  `video_desc` VARCHAR(500) NULL ,
-  `youtube_id` VARCHAR(50) NULL ,
-  PRIMARY KEY (`video_id`) )
-ENGINE = InnoDB;
+--
+-- Table structure for table `project_comments`
+--
 
+CREATE TABLE IF NOT EXISTS `project_comments` (
+  `project_comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_id` int(11) DEFAULT NULL,
+  `picture_id` int(11) DEFAULT NULL,
+  `story_id` int(11) DEFAULT NULL,
+  `video_id` int(11) DEFAULT NULL,
+  `audio_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`project_comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- -----------------------------------------------------
--- Table `mydb`.`project_story_picture`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`project_story_picture` (
-  `picture_id` INT NOT NULL ,
-  `story_id` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`picture_id`, `story_id`) ,
-  INDEX `project_story_pic_picfk` (`picture_id` ASC) ,
-  INDEX `project_story_pic_storyfk` () ,
-  CONSTRAINT `project_story_pic_picfk`
-    FOREIGN KEY (`picture_id` )
-    REFERENCES `mydb`.`picture` (`picture_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `project_story_pic_storyfk`
-    FOREIGN KEY ()
-    REFERENCES `mydb`.`project_story` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `project_story`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`project_story_audio`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`project_story_audio` (
-  `audio_id` INT NOT NULL ,
-  `story_id` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`audio_id`, `story_id`) ,
-  INDEX `projct_story_story_fk` () ,
-  INDEX `projct_story_audio_fk` (`audio_id` ASC) ,
-  CONSTRAINT `projct_story_story_fk`
-    FOREIGN KEY ()
-    REFERENCES `mydb`.`project_story` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `projct_story_audio_fk`
-    FOREIGN KEY (`audio_id` )
-    REFERENCES `mydb`.`audio` (`audio_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `project_story` (
+  `story_id` int(11) NOT NULL AUTO_INCREMENT,
+  `story_name` varchar(250) DEFAULT NULL,
+  `project_id` int(11) NOT NULL,
+  `village_id` int(11) NOT NULL,
+  `latitude` decimal(10,7) DEFAULT NULL,
+  `longitude` decimal(10,7) DEFAULT NULL,
+  `submitted_by` int(11) DEFAULT NULL,
+  `project_desc` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`story_id`),
+  KEY `village_id_fk` (`village_id`),
+  KEY `project_id_fk` (`project_id`),
+  KEY `submitted_by_fk` (`submitted_by`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `mydb`.`project_story_video`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`project_story_video` (
-  `video_id` INT NOT NULL ,
-  `story_id` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`video_id`, `story_id`) ,
-  INDEX `project_story_pic_picfk` (`video_id` ASC) ,
-  INDEX `project_story_pic_storyfk` () ,
-  CONSTRAINT `project_story_pic_videofk0`
-    FOREIGN KEY (`video_id` )
-    REFERENCES `mydb`.`video` (`video_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `project_story_pic_storyfk0`
-    FOREIGN KEY ()
-    REFERENCES `mydb`.`project_story` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Table structure for table `project_story_audio`
+--
 
+CREATE TABLE IF NOT EXISTS `project_story_audio` (
+  `audio_id` int(11) NOT NULL,
+  `story_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`audio_id`,`story_id`),
+  KEY `projct_story_audio_fk` (`audio_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- -----------------------------------------------------
--- Table `mydb`.`project_tags`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`project_tags` (
-  `project_tag_id` INT(11) NOT NULL ,
-  `tag_id` INT(11) NULL ,
-  `picture_id` INT(11) NULL ,
-  `story_id` INT(11) NULL ,
-  `video_id` INT(11) NULL ,
-  `audio_id` INT(11) NULL ,
-  `project_id` INT(11) NULL ,
-  PRIMARY KEY (`project_tag_id`) )
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `project_story_picture`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`project_comments`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`project_comments` (
-  `project_comment_id` INT(11) NOT NULL ,
-  `tag_id` INT(11) NULL ,
-  `picture_id` INT(11) NULL ,
-  `story_id` INT(11) NULL ,
-  `video_id` INT(11) NULL ,
-  `audio_id` INT(11) NULL ,
-  `project_id` INT(11) NULL ,
-  PRIMARY KEY (`project_comment_id`) )
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `project_story_picture` (
+  `picture_id` int(11) NOT NULL,
+  `story_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`picture_id`,`story_id`),
+  KEY `project_story_pic_picfk` (`picture_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `project_story_video`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE IF NOT EXISTS `project_story_video` (
+  `video_id` int(11) NOT NULL,
+  `story_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`video_id`,`story_id`),
+  KEY `project_story_pic_picfk` (`video_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_tags`
+--
+
+CREATE TABLE IF NOT EXISTS `project_tags` (
+  `project_tag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_id` int(11) DEFAULT NULL,
+  `picture_id` int(11) DEFAULT NULL,
+  `story_id` int(11) DEFAULT NULL,
+  `video_id` int(11) DEFAULT NULL,
+  `audio_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`project_tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `facebook_email` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video`
+--
+
+CREATE TABLE IF NOT EXISTS `video` (
+  `video_id` int(11) NOT NULL AUTO_INCREMENT,
+  `video_title` varchar(45) DEFAULT NULL,
+  `video_desc` varchar(500) DEFAULT NULL,
+  `youtube_id` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`video_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `village`
+--
+
+CREATE TABLE IF NOT EXISTS `village` (
+  `village_id` int(11) NOT NULL AUTO_INCREMENT,
+  `village_name` varchar(45) DEFAULT NULL,
+  `country_id` int(3) DEFAULT NULL,
+  PRIMARY KEY (`village_id`),
+  KEY `country_id_fk` (`country_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `project_story`
+--
+ALTER TABLE `project_story`
+  ADD CONSTRAINT `project_id_fk` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `submitted_by_fk` FOREIGN KEY (`submitted_by`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `village_id_fk` FOREIGN KEY (`village_id`) REFERENCES `village` (`village_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `project_story_audio`
+--
+ALTER TABLE `project_story_audio`
+  ADD CONSTRAINT `projct_story_audio_fk` FOREIGN KEY (`audio_id`) REFERENCES `audio` (`audio_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `project_story_picture`
+--
+ALTER TABLE `project_story_picture`
+  ADD CONSTRAINT `project_story_pic_picfk` FOREIGN KEY (`picture_id`) REFERENCES `picture` (`picture_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `project_story_video`
+--
+ALTER TABLE `project_story_video`
+  ADD CONSTRAINT `project_story_pic_videofk0` FOREIGN KEY (`video_id`) REFERENCES `video` (`video_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `village`
+--
+ALTER TABLE `village`
+  ADD CONSTRAINT `country_id_fk` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
